1a3b01c6296c046cb9ab660b206dc4d11afde960
gevget <gevget@gmail.com>
2026-06-16 19:42:43 +0300

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/tolk-usite/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
